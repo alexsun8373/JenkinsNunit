a780306594c20db2f4a1e041cf70469a8ec9ffd2
alexsun8373 <50131565+alexsun8373@users.noreply.github.com>
2019-05-09 12:49:43 +0800

    Add files via upload

--- BasicMaths/BasicMaths.cs
+++ BasicMaths/BasicMaths.cs
@@ -23,7 +23,7 @@ namespace BasicMath
         public double Multiply(double num1, double num2)
         {
             // To trace error while testing, writing + operator instead of * operator.  
-            return num1 * num2;
+            return num1 + num2;
         }
     }
 }
